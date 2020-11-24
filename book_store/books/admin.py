898eeb17da8741627120b14fb9980e2b676ca0b6
Pawan08082000 <59208833+Pawan08082000@users.noreply.github.com>
2020-11-24 22:29:01 +0530

    Removed unwanted files

--- book_store/books/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from books.models import *
-
-admin.site.register(Ureg)
-admin.site.register(Books)
-admin.site.register(Cate)
-admin.site.register(Cart)
-admin.site.register(Bcomm)
